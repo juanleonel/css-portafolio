0392ca401ca8e841c7ac87f2c34f0d7f4b3b60e3
Juan L. Albores <juanl.albores.c@gmail.com>
2020-04-14 10:41:27 -0500

    first commit

---
+++ assets/js/bootstrap.js
@@ -0,0 +1,3 @@
+// @koala-prepend "../../node_modules/jquery/dist/jquery.js"
+// @koala-prepend "../../node_modules/bootstrap/js/transition.js"
+// @koala-prepend "../../node_modules/jasny-bootstrap/js/offcanvas.js"
---
+++ assets/js/html5shiv.js
@@ -0,0 +1,1 @@
+// @koala-prepend "../../node_modules/html5shiv/dist/html5shiv.js"
